7e63187ac3760d283a3d5e5d9a93abef8399102a
Swatar Common <swatarcommon@gmail.com>
2020-10-20 14:06:30 +0300

    completed lab 8 and started lab 9

---
+++ Lab_9/Assets/First_script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First_script : MonoBehaviour
+{
+    public GameObject BlueCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, BlueCube.transform.position, Time.deltaTime);
+    }
+}
---
+++ Lab_9/Assets/Second_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Second_script : MonoBehaviour {
+    public float speed = 1.5f;
+    // Start is called before the first frame update
+    void Start() {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.mass = 7;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+
+        transform.position = new Vector3(x + (speed * Time.deltaTime), y, z);
+    }
+}
